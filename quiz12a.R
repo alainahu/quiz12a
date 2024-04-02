f7cfad675d38945e2135d5f81f624658fbd15613
Alaina Hu <alaina.hu@mail.utoronto.ca>
2024-04-02 17:52:43 -0400

    Added all code into script.

---
+++ quiz12a.R
@@ -0,0 +1,69 @@
+#### Preamble ####
+# Purpose: Simulates the data for deaths attributed to cancer for 5 hospitals
+# Author: Alaina Hu 
+# Date: 1 April 2024 
+# Contact: alaina.hu@utoronto.ca 
+# License: MIT
+# Pre-requisites: NA
+# Any other information needed? NA
+
+
+#### Workspace setup ####
+
+library(tidyverse)
+library(ggplot2)
+
+
+#### Simulate data ####
+set.seed(853)
+
+number_of_years <- 20
+start_mean <- 80 
+decrease_per_year <- 2 
+sd_deaths <- 2 
+
+mean_deaths_per_year <- start_mean - (0:(number_of_years - 1)) * decrease_per_year
+
+simulated_data <-
+  tibble(
+    hospital = rep(paste("Hospital", 1:5), each = number_of_years),
+    year = rep(2003:(2002 + number_of_years), times = 5),
+    deaths = round(rnorm(n = number_of_years * 5, mean = rep(mean_deaths_per_year, each = 5), sd = sd_deaths))
+  )
+
+simulated_data_sorted <- simulated_data %>%
+  arrange(year)
+
+head(simulated_data_sorted)
+
+#### Plot data ####
+simulated_data_sorted |>
+  ggplot(aes(x = year, y = deaths, color = hospital)) +
+  geom_point() +
+  theme_minimal() +
+  labs(x = "Year", y = "Cancer Deaths", color = "Hospital") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
+
+#### Test data ####
+hospital_names <- c("Hospital 1", "Hospital 2", "Hospital 3", "Hospital 4", "Hospital 5")
+
+simulated_data_sorted$hospital |>
+  unique() %in% hospital_names
+
+simulated_data_sorted$hospital |> class() == "character"
+
+simulated_data_sorted$year |> class() == "integer"
+simulated_data_sorted$year |> min() == 2003
+simulated_data_sorted$year |> max() == 2023
+
+simulated_data_sorted$year |> unique() |> length() == 20
+
+simulated_data_sorted$deaths |> class() == "numeric"
+simulated_data_sorted$deaths |> min() >= 0
+simulated_data_sorted$deaths |> max() <= 200
+
+simulated_data_sorted$hospital |> length() == 100
+
+
+
