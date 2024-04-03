57a2d3bc0c9e797815e52258de0abd9774af4212
Alaina Hu <alaina.hu@mail.utoronto.ca>
2024-04-02 22:00:19 -0400

    Created model.

--- quiz12a.R
+++ quiz12a.R
@@ -17,6 +17,8 @@ library(ggplot2)
 #### Simulate data ####
 set.seed(853)
 
+age_groups <- c("<40", "40-60", ">60")
+cancer_types <- c("Breast", "Lung", "Prostate", "Colorectal", "Skin")
 number_of_years <- 20
 start_mean <- 80 
 decrease_per_year <- 2 
@@ -28,6 +30,8 @@ simulated_data <-
   tibble(
     hospital = rep(paste("Hospital", 1:5), each = number_of_years),
     year = rep(2003:(2002 + number_of_years), times = 5),
+    age_group = sample(age_groups, size = 100, replace = TRUE),
+    cancer_type = sample(cancer_types, size = 100, replace = TRUE),
     deaths = round(rnorm(n = number_of_years * 5, mean = rep(mean_deaths_per_year, each = 5), sd = sd_deaths))
   )
 
@@ -53,6 +57,10 @@ simulated_data_sorted$hospital |>
 
 simulated_data_sorted$hospital |> class() == "character"
 
+simulated_data_sorted$age_group |> unique() %in% age_groups
+
+simulated_data_sorted$cancer_type |> unique() %in% cancer_types
+
 simulated_data_sorted$year |> class() == "integer"
 simulated_data_sorted$year |> min() == 2003
 simulated_data_sorted$year |> max() == 2023
@@ -65,5 +73,29 @@ simulated_data_sorted$deaths |> max() <= 200
 
 simulated_data_sorted$hospital |> length() == 100
 
+#### Model ####
+library(rstanarm)
 
+model <-
+  stan_glm(
+    deaths ~ age_group,
+    data = simulated_data_sorted,
+    family = poisson(link = "log"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    seed = 853
+  )
 
+saveRDS(
+  model,
+  file = "model.rds"
+)
+
+library(modelsummary)
+modelsummary::modelsummary(
+  list(
+    "Poisson" = model
+  ),
+  statistic = "conf.int",
+  fmt = 2
+)
